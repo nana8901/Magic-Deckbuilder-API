034a26ba229e965105a5d67e334eca69e8d5868d
Nathan Brownstein <nana8901@gmail.com>
2022-09-09 14:52:15 -0400

    added basic hardcoded show into card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,14 @@
 class CardsController < ApplicationController
   def add_to_deck
+    add = cards_in_deck.new(
+      deck_id: 1,
+      card_id: 1,
+      number_in_deck: 1,
+    )
   end
   def show
+    card = Card.find_by(id: 1)
+    render json: card
   end
   def index
     @cards = Card.all
